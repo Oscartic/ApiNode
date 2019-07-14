50dac49be948c3f169aee2b5e31d8bfc43b2518a
OscarB <oballesteros@abstract.cl>
2019-07-13 21:25:50 -0400

    Model income, IncomeController and routes

---
+++ routes/income.js
@@ -0,0 +1,13 @@
+import routerx from 'express-promise-router'
+import incomeController from '../controllers/IncomeController'
+import auth from '../middlewares/auth'
+
+const router= routerx();
+
+router.post('/add', auth.verifyStorekeeper, incomeController.add)
+router.get('/query', auth.verifyStorekeeper, incomeController.query)
+router.get('/list', auth.verifyStorekeeper, incomeController.list)
+router.put('/activate', auth.verifyStorekeeper, incomeController.activate)
+router.put('/deactivate', auth.verifyStorekeeper, incomeController.deactivate)
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ import categoryRouter from './category'
 import articleRouter from './article'
 import userRouter from './user'
 import personRouter from './person'
+import incomeRouter from './income'
 
 const router= routerx();
 router.use('/category', categoryRouter)
 router.use('/article', articleRouter)
 router.use('/user', userRouter)
 router.use('/person', personRouter)
+router.use('./income', incomeRouter)
+
 export default router
